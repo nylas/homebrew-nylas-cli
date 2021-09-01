c7cfbd5f2656f3d50d7f5eaef5d2bf99bcd41c4e
nylas-ci <support@nylas.com>
2021-09-01 17:34:21 +0000

    Updated to the latest version

--- nylas.rb
+++ nylas.rb
@@ -5,28 +5,28 @@
 class Nylas < Formula
   desc "Nylas Command Line Utility"
   homepage "https://nylas.com"
-  version "1.0.0-beta1"
+  version "1.0.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://download.nylas.com/nylas-cli/v1.0.0-beta1/nylas-cli_1.0.0-beta1_Darwin_x86_64.tar.gz"
-      sha256 "923a085f3569bdd979ee5a7e94534cda3a038dae640cf8a374d363eb8291a596"
+      url "https://download.nylas.com/nylas-cli/v1.0.0/nylas-cli_1.0.0_Darwin_x86_64.tar.gz"
+      sha256 "7c8c8acb0a22bd72474b369305b573817d87282ec063995b060e4a372a60b42c"
     end
     if Hardware::CPU.arm?
-      url "https://download.nylas.com/nylas-cli/v1.0.0-beta1/nylas-cli_1.0.0-beta1_Darwin_arm64.tar.gz"
-      sha256 "5346f7e963521dad5fbe7fdd3e42399b6038f6bcd5601ffa2171a608ad9447e2"
+      url "https://download.nylas.com/nylas-cli/v1.0.0/nylas-cli_1.0.0_Darwin_arm64.tar.gz"
+      sha256 "c781ae4253c5d4d8fbacf8f1169002e4bffcc118222eb9db419913043135cfe4"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://download.nylas.com/nylas-cli/v1.0.0-beta1/nylas-cli_1.0.0-beta1_Linux_x86_64.tar.gz"
-      sha256 "451e93711e9da9d3a16c89599fb7f63df18c66241c9135d4e82df3d904daa145"
+      url "https://download.nylas.com/nylas-cli/v1.0.0/nylas-cli_1.0.0_Linux_x86_64.tar.gz"
+      sha256 "4815aeb64a5aa25639bb85021f7705366910e0e67274126d1014e0d34a138fb4"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://download.nylas.com/nylas-cli/v1.0.0-beta1/nylas-cli_1.0.0-beta1_Linux_arm64.tar.gz"
-      sha256 "35dcc24a4376acaddf46f265d362ab0290f55c3755a333cf79d9c15949ed8cf7"
+      url "https://download.nylas.com/nylas-cli/v1.0.0/nylas-cli_1.0.0_Linux_arm64.tar.gz"
+      sha256 "2ae1421389274c39fa81964207c7d46d609a569a92a17b76d9994a031e1ac184"
     end
   end
 
